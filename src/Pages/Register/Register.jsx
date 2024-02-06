c92f6c30bac67bb5670d9173814f5ac33130102b
GabrielEscudillo <gabrielescudillo@gmail.com>
2024-02-06 23:12:39 +0100

    added register fields

--- src/Pages/Register/Register.jsx
+++ src/Pages/Register/Register.jsx
@@ -1,13 +1,70 @@
 import { CustomInput } from "../../Components/CustomInput/CustomInput";
+import "./Register.css";
+import { useEffect, useState } from "react";
 
 export const Register = () => {
+  const [registerData, setRegisterData] = useState({
+    name: "",
+    last_name: "",
+    address: "",
+    email: "",
+    password: "",
+    phone_number: "",
+  });
+
+  const inputHandler = (event) => {
+    setRegisterData((prevState) => ({
+        ...prevState,
+        [event.target.name]: event.target.value,
+    }));
+};
+console.table(registerData);
   return (
     <>
-      <div className="home">
-        <h1 className="title">
-          <div className="miDiv"></div>Welcome to <br />
-          tattoink
-        </h1>    
+      <div className="body">
+        <div className="requiredFields">
+          <CustomInput
+            placeholder={"name"}
+            type={"text"}
+            name={"name"}
+            handler={inputHandler}
+          ></CustomInput>
+          <CustomInput
+            placeholder={"last_name"}
+            type={"text"}
+            name={"last_name"}
+            handler={inputHandler}
+          ></CustomInput>
+          <CustomInput
+            placeholder={"address"}
+            type={"text"}
+            name={"address"}
+            handler={inputHandler}
+          ></CustomInput>
+          <CustomInput
+            placeholder={"email"}
+            type={"email"}
+            name={"email"}
+            handler={inputHandler}
+          ></CustomInput>
+          <CustomInput
+            placeholder={"password"}
+            type={"password"}
+            name={"password"}
+            handler={inputHandler}
+          ></CustomInput>
+          <CustomInput
+            placeholder={"phone_number"}
+            type={"phone_number"}
+            name={"phone_number"}
+            handler={inputHandler}
+          ></CustomInput>
+        </div>
+        <input
+          type="submit"
+          onClick={buttonHandler}
+          value="Register"
+        ></input>
       </div>
     </>
   );
--- src/Services/apiCalls.js
+++ src/Services/apiCalls.js
@@ -11,34 +11,9 @@ export const userLogin = async (credentials) => {
         console.error('Error en el login:', error);
     throw error;
     }
-    
 }
 
-// export const authenticateUser = async (credentials) => {
-//     console.log(credentials); const res = await axios.post(`${API_URL}/api/login`, credentials);
-   
-//     return res;
-// }
-
-
-// // export const authenticateUser = async (credentials) => {
-// //     try {
-// //         if (!credentials.email || !credentials.password) {
-// //             throw new Error('Both email and password are required.');
-// //         }
-
-// //         const res = await axios.post(`${API_URL}/api/login`, credentials);
-// //         // const token = res;
-// //         return res;
-// //     } catch (error) {         
-// //         console.log(credentials)
-// //         console.error('Error en el login:', error);
-// //         console.log('Server response:', error);
-// //         throw error;
-// //     }
-    
-// // }
-
-
-
-
+export const Register = async (credentials) => {
+    const res = await axios.post('${API_URL}/api/register', credentials, {})
+    return ('home')
+}
